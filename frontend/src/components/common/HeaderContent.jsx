b8680cafe1229aa60e3f647fcc641c8738d39f90
CHOINEON <zhdlf724@naver.com>
2025-11-30 22:27:32 +0900

    🚧 Header Title letters cutting off issue

--- frontend/src/components/common/HeaderContent.jsx
+++ frontend/src/components/common/HeaderContent.jsx
@@ -1,38 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
-
 import gradientBar from '@/assets/icons/gradientBar.svg';
 
-const HeaderContentContainer = styled.div`
-    flex-grow: 1; 
+const HeaderContainer = styled.div`
+
+    height: 100%; 
     display: flex;
     align-items: center;
-    // justify-content: space-between;
-    height: 100%;
-   
+    padding: 0 20px;
     color: white; 
     font-weight: bold;
-    padding-right: 20px;
+    font-size: 1.2em;
     
-    // 이전에 Header에 넣기로 했던 배경 스타일을 여기서 직접 적용하지 않고
-    // MainLayout에서 Header 컴포넌트 전체에 스타일링을 적용하거나,
-    // 이 컨텐츠 자체에 Styled Components를 적용하는 것이 더 좋습니다.
-    
-    // 임시 스타일: 배경 이미지를 antd Header에 적용하는 대신,
-    // 제목만 표시하도록 간단하게 구현합니다.
+
     background-image: url(${gradientBar});
-    background-size: cover;
+    background-size: cover; 
     background-repeat: no-repeat;
+    background-position: center;
 `;
 
 const HeaderContent = ({ title }) => {
     return (
-        <HeaderContentContainer>
-            {/* 상단 제목 */}
-            <div>{title}</div>
-            
-            {/* 테마 토글 등 추가 요소 */}
-        </HeaderContentContainer>
+        <HeaderContainer>
+            {title}
+        </HeaderContainer>
     );
 };
 
